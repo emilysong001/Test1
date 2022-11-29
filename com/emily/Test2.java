5ef19f08789df03ee4d64d7b8c9251366461cb24
Xuanxuan Song <emily001.song@gmail.com>
2022-11-29 12:56:48 -0500

    add 2 java file

---
+++ com/emily/Test2.java
@@ -0,0 +1,8 @@
+package com.emily;
+
+public class Test2 {
+  public static main(String[] args){
+  System.out.println("hello world 2");
+  }
+
+}
---
+++ com/emily/Test3.java
@@ -0,0 +1,8 @@
+package com.emily;
+
+public class Test3 {
+  public static main(String[] args){
+  System.out.println("hello world 3");
+  }
+
+}
